0138e420725ab31d379b4f313b5f258cba479b6e
Azhar <mohdazharsm@gmail.com>
2025-09-26 22:07:26 +0530

    Refactor robot configuration and launch files for improved control and integration

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -3,9 +3,16 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
     return LaunchDescription([
-
+        # Static transform publisher for laser frame
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='laser_static_transform_publisher',
+        #     arguments=['0.122', '0', '0.212', '0', '0', '0', 'chassis', 'laser_frame']
+        # ),
+        
+        # RPLIDAR node
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
